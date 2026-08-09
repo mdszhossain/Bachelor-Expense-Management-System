a549df8f75d586e56021df245d8a6229913a92fb
mdszhossain <personalmailshazzad@gmail.com>
2026-08-09 22:06:01 +0600

    Render signin page from server side programming.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,6 @@
 module.exports.renderSignupPage = async(req, res) => {
     res.render("signup.ejs")
 }
+module.exports.renderSigninPage = async(req, res) => {
+    res.render("signin.ejs");
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.get("/signup", userController.renderSignupPage);
+router.get("/signin", userController.renderSigninPage);
 
 module.exports = router;
